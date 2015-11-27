4c46cc7450a81da99ade9fe44ebcd36bb06ad33d
gerifield <radics.gergely@ustream.tv>
2015-11-27 12:43:50 +0100

    Added Get tests

--- heartbeat_test.go
+++ heartbeat_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -15,6 +16,21 @@ type testHandler struct {
 
 func (t testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	handler(w, r)
+}
+
+type testGetHandler struct {
+	JsonReturn string
+	HttpError  string
+	HttpCode   int
+}
+
+func (t testGetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if t.HttpError != "" {
+		http.Error(w, t.HttpError, t.HttpCode)
+	} else {
+		w.Write([]byte(t.JsonReturn))
+	}
+
 }
 
 func init() {
@@ -52,24 +68,68 @@ func TestHandler(t *testing.T) {
 		}
 
 		if hm.Status != "running" {
-			t.Fatal(errors.New("The server should running"))
+			t.Error(errors.New("The server should running"))
 		}
 
 		if hm.Build != tv.HashResult {
-			t.Fatal(errors.New("Wrong commit hash"))
+			t.Error(errors.New("Wrong commit hash"))
 		}
 
 		uptime, err := time.ParseDuration(hm.Uptime)
 		if err != nil {
-			t.Fatal(err)
+			t.Error(err)
 		}
 
 		if uptime > time.Since(StartTime) {
-			t.Fatal(errors.New("Wrong uptime"))
+			t.Error(errors.New("Wrong uptime"))
 		}
 	}
 }
 
-func TestHGet(t *testing.T) {
+func TestGet(t *testing.T) {
 
+	var TestValues = []struct {
+		Json      string
+		HttpError string
+		HttpCode  int
+		RetMsg    HeartbeatMessage
+		RetErr    string
+	}{
+		{
+			`{"status":"running","build":"testHash","uptime":"5m31.5s"}`,
+			"",
+			http.StatusOK,
+			HeartbeatMessage{"running", "testHash", "5m31.5s"},
+			"",
+		},
+		{
+			"",
+			"Error",
+			http.StatusBadRequest,
+			HeartbeatMessage{},
+			"Wrong status code: 400",
+		},
+		{
+			"",
+			"",
+			http.StatusOK,
+			HeartbeatMessage{},
+			"Error occured unmarshalling the response",
+		},
+	}
+
+	for _, tv := range TestValues {
+
+		ts := httptest.NewServer(testGetHandler{tv.Json, tv.HttpError, tv.HttpCode})
+		defer ts.Close()
+
+		hm, err := Get(ts.URL)
+		if err != nil && err.Error() != tv.RetErr {
+			t.Fatal("Wrong error result! Expected:", tv.RetErr, "Got:", err)
+		}
+
+		if !reflect.DeepEqual(hm, tv.RetMsg) {
+			t.Fatal("Wrong result object! Expected:", tv.RetMsg, "Got:", hm)
+		}
+	}
 }
